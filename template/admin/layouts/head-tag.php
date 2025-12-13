ec18b89656ba5edc555dee896e2d2d6808f7b303
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2025-12-13 13:54:29 +0700

    giao diện auth và admin

--- template/admin/layouts/head-tag.php
+++ template/admin/layouts/head-tag.php
@@ -21,15 +21,15 @@
 
 <body>
 
-    <nav class="navbar  navbar-light bg-gradiant-green-blue nav-shadow">
+    <nav class="navbar  navbar-light bg-gradiant-purple nav-shadow">
 
         <a class="navbar-brand" href=""></a>
         <span class="">
-   </a>
+            </a>
             <span class="dropdown">
                 <a class="dropdown-toggle text-decoration-none text-dark" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i class="fas fa-user"></i>
-                 
+
                 </a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item" href="<?= url('logout') ?>">Logout</a>
@@ -41,14 +41,14 @@
     <div class="container-fluid">
         <div class="row">
             <nav class="col-md-2 d-none d-md-block pt-3 bg-sidebar sidebar px-0">
-                <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin') ?>"><i class="fas fa-home"></i> Home</a>
+                <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin') ?>"><i class="fas fa-home"></i> Dashboard</a>
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/category') ?>"><i class="fas fa-clipboard-list"></i> Category</a>
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/post') ?>"><i class="fas fa-newspaper"></i> Post</a>
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/banner') ?>"><i class="fas fa-image"></i> Banner</a>
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/comment') ?>"><i class="fas fa-comments"></i> Comment</a>
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/menu') ?>"><i class="fas fa-bars"></i> Menus</a>
                 <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/user') ?>"><i class="fas fa-users"></i> User</a>
-            <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/web-setting') ?>"><i class="fas fa-tools"></i> Web Setting</a>
+                <a class="text-decoration-none d-block py-1 px-2 mt-1" href="<?= url('admin/web-setting') ?>"><i class="fas fa-tools"></i> Web Setting</a>
             </nav>
 
             <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
